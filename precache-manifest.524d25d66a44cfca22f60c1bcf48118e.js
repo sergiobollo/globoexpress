20caffacfa403789104f785a082b954aa0d503bc
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-04-01 22:56:03 -0300

    Updates

---
+++ precache-manifest.524d25d66a44cfca22f60c1bcf48118e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30035d5e9d41cb0238a2cba65e7e421f",
+    "url": "/globoexpress/index.html"
+  },
+  {
+    "revision": "e315e736bdcab792b6ae",
+    "url": "/globoexpress/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fd9badc071baaff92495",
+    "url": "/globoexpress/static/css/main.3e383edb.chunk.css"
+  },
+  {
+    "revision": "e315e736bdcab792b6ae",
+    "url": "/globoexpress/static/js/2.527436a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/globoexpress/static/js/2.527436a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9badc071baaff92495",
+    "url": "/globoexpress/static/js/main.c62fcf7d.chunk.js"
+  },
+  {
+    "revision": "11703fe029a90c154ed0",
+    "url": "/globoexpress/static/js/runtime-main.8554fc6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e46751696b7c904031d53cc83360a24f.js"
+  "/globoexpress/precache-manifest.524d25d66a44cfca22f60c1bcf48118e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/globoexpress/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
